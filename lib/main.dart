65ce1750341f6dc92e54e920aaabc6ea2f19e2de
nivyragavan <nivyragavan@gmail.com>
2023-01-02 11:19:22 +0530

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
-      title: 'Dashboard',
+      title: 'Sub-Admin Dashboard',
       theme: ThemeData(
         scaffoldBackgroundColor: light,
         textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
